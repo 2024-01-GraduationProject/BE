5a45fa72f4f1640bb84b6f8de7e67863cbfedd2c
Seohyeon.Lim <forestlight749@gmail.com>
2024-06-27 23:30:18 +0900

    init 프로젝트 생성

---
+++ src/main/java/BookMind/GraduationProject_BE/GraduationProjectBeApplication.java
@@ -0,0 +1,13 @@
+package BookMind.GraduationProject_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BookMind/GraduationProject_BE/GraduationProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package BookMind.GraduationProject_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
