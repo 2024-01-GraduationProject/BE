f759940065fe56fa105272c9cfa448fa9a63412e
Seohyeon.Lim <forestlight749@gmail.com>
2024-07-06 15:00:32 +0900

    CORS 설정 수정

--- src/main/java/BookMind/GraduationProject_BE/Controller/MemberController.java
+++ src/main/java/BookMind/GraduationProject_BE/Controller/MemberController.java
@@ -5,16 +5,9 @@ import BookMind.GraduationProject_BE.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000") // CORS 설정
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
