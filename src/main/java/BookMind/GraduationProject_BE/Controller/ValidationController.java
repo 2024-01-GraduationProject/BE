b73e59ba4ceeeffe47a8d1e3e4f7e3dd2b86bf34
Seohyeon.Lim <forestlight749@gmail.com>
2024-07-29 19:28:09 +0900

    마이페이지 > 회원정보 수정 > 현재 비밀번호 확인 코드

--- src/main/java/BookMind/GraduationProject_BE/Controller/ValidationController.java
+++ src/main/java/BookMind/GraduationProject_BE/Controller/ValidationController.java
@@ -2,12 +2,15 @@ package BookMind.GraduationProject_BE.Controller;
 
 import BookMind.GraduationProject_BE.DTO.EmailRequest;
 import BookMind.GraduationProject_BE.DTO.NicknameRequest;
+import BookMind.GraduationProject_BE.DTO.PasswordRequest;
 import BookMind.GraduationProject_BE.Service.ValidationService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -36,5 +39,21 @@ public class ValidationController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("isDuplicate", isDuplicate);
         return ResponseEntity.ok(response);
+    }
+
+    // 마이페이지 _ 회원정보 수정 페이지
+    // 변경 전(현재) 비밀번호 확인
+    @PostMapping("check-password")
+    public ResponseEntity<?> checkingPassword(HttpSession session, @RequestBody PasswordRequest passwordRequest) {
+        String inputPassword = passwordRequest.getCurrentPassword();
+        // 1. 현재 사용자의 비밀번호 정보를 받아옴
+        String userPassword = (String) session.getAttribute("loginPassword");
+
+        // 2. 사용자가 입력한 비밀번호와 현재 사용자의 비밀번호의 일치 여부를 확인
+        if (inputPassword.equals(userPassword)) {
+            return ResponseEntity.ok("비밀번호가 일치합니다.");
+        } else {
+            return ResponseEntity.badRequest().body("비밀번호가 일치하지 않습니다.");
+        }
     }
 }
